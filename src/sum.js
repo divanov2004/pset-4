13063ac88a42a051ee9bd094625953c3cbe20cd5
divanov2004 <55397915+divanov2004@users.noreply.github.com>
2019-11-11 10:38:32 -0500

    sum.js

---
+++ src/sum.js
@@ -0,0 +1,15 @@
+const readlineSync = require("readline-sync");
+
+const userLowerBound = readlineSync.question("\nLower bound: ");
+const userUpperBound = readlineSync.question("Upper bound: ");
+
+console.log("Lower bound: " + userUpperBound);
+console.log("Upper bound: " + userLowerBound);
+
+
+
+
+
+
+
+console.log("\n"):
