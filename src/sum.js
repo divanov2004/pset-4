afc26f212fdb08996ebc36d98c916d3843be4a5e
divanov2004 <55397915+divanov2004@users.noreply.github.com>
2019-11-15 09:47:39 -0500

    finished sum.js started reverse.js

--- src/sum.js
+++ src/sum.js
@@ -1,14 +1,34 @@
 const readlineSync = require("readline-sync");
 
-const lowerBound = readlineSync.question("\nLower bound: ");
-const upperBound = readlineSync.question("Upper bound: ");
+let lowerBound = Number(readlineSync.question("\nLower bound: "));
+let upperBound = Number(readlineSync.question("Upper bound: "));
 
-let evenSum = 0
+const MIN = Number.MIN_SAFE_INTEGER;
+const MAX = Number.MAX_SAFE_INTEGER;
+let sum = 0;
+let i = 0;
 
-let MIN = Number.MIN_SAFE_INTEGER;
-let MAX = Number.MAX_SAFE_INTEGER;
+do {
+   lowerBound = Number(readlineSync.question("Lower bound: "));
+   upperBound = Number(readlineSync.question("Upper bound: "));
+} while (lowerBound > upperBound);
 
-while (lowerBound > upperBound) {
-  lowerBound = Number(readlineSync.question("Lower bound: "));
-  upperBound = Number(readlineSync.question("Upper bound: "));
+if (lowerBound % 2 === 0) {
+  for (let i = lowerBound; i <= upperBound; i = i + 2){
+    sum = i + sum;
+  }
 }
+
+else if (lowerBound % 2 !== 0) {
+  for (let i = lowerBound + 1; i <= upperBound; i = i + 2){
+    sum = i + sum;
+  }
+}
+
+function thousands_separators(num)
+  {
+    var num_parts = num.toString().split(".");
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return num_parts.join(".");
+  }
+  console.log(thousands_separators("\n" + sum + "."));
