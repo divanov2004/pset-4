ebfda8a6eeaa771d7feee6caa29ed9ecf3c41252
divanov2004 <55397915+divanov2004@users.noreply.github.com>
2019-11-21 10:48:14 -0500

    yeet

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,1 +1,4 @@
 const readlineSync = require("readline-sync");
+
+const MIN = 1
+const MAX = 78
--- src/odd.js
+++ src/odd.js
@@ -1,1 +1,4 @@
 const readlineSync = require("readline-sync");
+
+const MIN = Number.MIN_SAFE_INTEGER;
+const MAX = Number.MAX_SAFE_INTEGER;
